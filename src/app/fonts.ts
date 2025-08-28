a60d2f789e9bdaefe93d56c1fbc28352216adde7
Sana Khuram <sana.khuram.baig@gmail.com>
2025-08-28 10:59:16 +0200

    added color variables, updated layout, added nav and footer in layout

---
+++ src/app/fonts.ts
@@ -0,0 +1,16 @@
+// app/fonts.ts
+import { Poppins, Playfair_Display } from "next/font/google";
+
+export const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-poppins",
+    display: "swap",
+});
+
+export const playfair = Playfair_Display({
+    subsets: ["latin"],
+    weight: ["400", "600", "700"],
+    variable: "--font-playfair",
+    display: "swap",
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { poppins } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Holidaze | Accommodation Booking",
@@ -18,14 +10,17 @@ export const metadata: Metadata = {
     "Holidaze is a modern accommodation booking platform where users can find, book, and manage holiday venues. Venue managers can also create and manage their listings with ease.",
 };
 
-export default function RootLayout({
-  children,
+export default function RootLayout({ children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} ${poppins.variable} min-h-screen flex flex-col bg-background text-foreground`}>
+        <Navbar/>
+        <main className="flex-1">{children}</main>
+        <Footer/>
+        </body>
     </html>
   );
 }
