49396074c33468a4bdc4433527af229eefc78101
Sana Khuram <sana.khuram.baig@gmail.com>
2025-09-02 11:17:48 +0200

    🏡 Update image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+<<<<<<< Updated upstream
       { protocol: 'https', hostname: '**' }, 
     ],
   },
+=======
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "cdn.myapi.com" },
+    ],
+  },
+  experimental: {
+    typedRoutes: true, 
+  },
+>>>>>>> Stashed changes
 };
 
 export default nextConfig;
